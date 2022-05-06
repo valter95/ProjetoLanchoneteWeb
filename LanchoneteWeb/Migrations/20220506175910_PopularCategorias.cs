b4bca8f32225290545be46c8d2234ece344ea0b7
Valter <valterhenrique39@gmail.com>
2022-05-06 16:24:05 -0300

    Criado os migrations para criação e populamento das respectivas tabelas

---
+++ LanchoneteWeb/Migrations/20220506175910_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchoneteWeb.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+                "VALUES('Normal', 'Lanchefeito com ingredientes Normais') ");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+                "VALUES('Natural', 'Lanchefeito com ingredientes integrais e naturais') ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
