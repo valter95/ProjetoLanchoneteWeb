51f5aa07f065ee036afa44136014a083636ff3c3
Valter <valterhenrique39@gmail.com>
2022-06-29 16:48:04 -0300

    Criado area de checkout e pagina de informação do cliente, criado checkout completo com a finalização do pedido.

---
+++ LanchoneteWeb/Controllers/PedidoController.cs
@@ -0,0 +1,68 @@
+﻿using LanchoneteWeb.Models;
+using LanchoneteWeb.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchoneteWeb.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) 
+        {
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+            //obtem os itens do carrinho de compra do cliente 
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoDeCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //verificar se existem itens de pedido
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0) 
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+
+            foreach (var item in items) 
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+
+            //valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes 
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu Pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do Cliente 
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+        }
+    }
+}
